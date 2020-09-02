e835756cb439f0deda13516e3e5e020a74b1e055
Matt <matthillwakatipu@gmail.com>
2020-09-02 22:58:00 +1200

    Problem 22 complete.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,60 @@
-import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BigInteger limit = (new BigInteger("10")).pow(999);
-        BigInteger[] fibNumbers = new BigInteger[3];
+        long totalScore = 0;
 
-        fibNumbers[0] = BigInteger.ONE;
-        fibNumbers[2] = BigInteger.ONE;
+        ArrayList<String> names = readNamesFromFile();
 
-        int i = 0;
-        int n = 2;
+        Collections.sort(names);
 
-        while ((fibNumbers[i]).compareTo(limit) < 0) {
-            i = (i + 1) % 3;
-            fibNumbers[i] = fibNumbers[(i + 1) % 3].add(fibNumbers[(i + 2) % 3]);
-            n++;
+        int namesSize = names.size();
+
+        for (int i = 0; i < namesSize; i++) {
+            String name = names.get(i);
+            totalScore += calculateNameScore(name, i + 1);
         }
 
-        System.out.println(n);
+        System.out.println(totalScore);
+    }
+
+
+    public static int calculateNameScore(String name, int alphabeticalPosition) {
+        int sumOfLetters = 0;
+
+        for (int i = 0; i < name.length(); i++) {
+            sumOfLetters += name.charAt(i) - 64;
+        }
+
+        return sumOfLetters * alphabeticalPosition;
+    }
+
+
+    public static ArrayList<String> readNamesFromFile() {
+        ArrayList<String> names = new ArrayList<>();
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("p022_names.txt"));
+            Scanner fileScanner = new Scanner(bufferedReader);
+            String fileAsString = fileScanner.next();
+            fileAsString = fileAsString.substring(1, fileAsString.length() - 1);
+
+            Scanner scanner = new Scanner(fileAsString);
+            scanner.useDelimiter("\",\"");
+
+            //Add all names to ArrayList
+            while (scanner.hasNext()) {
+                names.add(scanner.next());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return names;
     }
 }
