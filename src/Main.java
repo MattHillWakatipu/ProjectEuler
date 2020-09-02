abe36a4a8d7d5502f621a07c72d326575b0b0f43
Matt <matthillwakatipu@gmail.com>
2020-09-02 22:25:29 +1200

    Problem 25 complete.

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int sum = 0;
-        int power = 1000;
-        BigInteger number = BigInteger.TWO;
-        number = number.pow(power);
+        BigInteger limit = (new BigInteger("10")).pow(999);
+        BigInteger[] fibNumbers = new BigInteger[3];
 
-        String numberAsString = number.toString();
+        fibNumbers[0] = BigInteger.ONE;
+        fibNumbers[2] = BigInteger.ONE;
 
-        for (int i = 0; i < numberAsString.length(); i++) {
-            sum += Character.getNumericValue(numberAsString.charAt(i));
+        int i = 0;
+        int n = 2;
+
+        while ((fibNumbers[i]).compareTo(limit) < 0) {
+            i = (i + 1) % 3;
+            fibNumbers[i] = fibNumbers[(i + 1) % 3].add(fibNumbers[(i + 2) % 3]);
+            n++;
         }
-        System.out.println(sum);
+
+        System.out.println(n);
     }
 }
