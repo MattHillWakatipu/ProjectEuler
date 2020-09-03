32cce7565d53b08cad287f6fcb31b96807fc9b6a
Matt <matthillwakatipu@gmail.com>
2020-09-03 22:33:06 +1200

    Restored Problem_13

--- src/ProjectEuler.java
+++ src/ProjectEuler.java
@@ -1,12 +1,13 @@
 import Problems_10_19.Problem_10;
 import Problems_10_19.Problem_11;
 import Problems_10_19.Problem_12;
+import Problems_10_19.Problem_13;
 
 public class ProjectEuler {
 
     public static void main(String[] args) {
 
-        Problem_12 problem = new Problem_12();
+        Problem_13 problem = new Problem_13();
         System.out.println(problem.calculateResult());
     }
 }
