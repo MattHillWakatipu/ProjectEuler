fb5162556e750240504994a8b871122099ed7018
Matt <matthillwakatipu@gmail.com>
2020-09-05 18:49:59 +1200

    Created Problem_030

---
+++ src/EulerProblems/Problems_030_039/Problem_030.java
@@ -0,0 +1,11 @@
+package EulerProblems.Problems_030_039;
+
+import EulerProblems.EulerProblem;
+
+public class Problem_030 implements EulerProblem {
+
+    @Override
+    public String calculateResult() {
+        return null;
+    }
+}
