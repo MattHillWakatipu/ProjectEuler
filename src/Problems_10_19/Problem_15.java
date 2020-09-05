6e7db73e8758ef6e84f14a7fa2386ba1d3b7d526
Matt <matthillwakatipu@gmail.com>
2020-09-05 17:27:27 +1200

    Problem_15 complete.

--- src/Problems_10_19/Problem_15.java
+++ src/Problems_10_19/Problem_15.java
@@ -1,7 +1,23 @@
 package Problems_10_19;
 
+/**
+ * Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down,
+ * there are exactly 6 routes to the bottom right corner.
+ *
+ * How many such routes are there through a 20×20 grid?
+ */
 public class Problem_15 {
-    public boolean calculateResult() {
-        return false;
+
+    public long calculateResult() {
+
+        int n = 20;
+        long result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result *= (i + n);
+            result /= i;
+        }
+
+        return result;
     }
 }
